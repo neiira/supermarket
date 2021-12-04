9b2ca842e679a91907992dc522c1732d1a3b07dc
Nira Tamang <neera678@gmail.com>
2021-12-04 23:03:51 +0545

    final update

--- src/views/Information.jsx
+++ src/views/Information.jsx
@@ -3,6 +3,7 @@ import BreadCrumb from '../Components/BreadCrumb';
 import { Form, Button } from 'react-bootstrap';
 import { userDetails } from '../actions/userActions';
 import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../Components/Loader';
 import '../styles/profile.css';
 
 function Information() {
@@ -24,37 +25,41 @@ function Information() {
           <div className="information-content">
             <h5>My account information</h5>
             <div className="pt-3">
-              {!loading && (
-                <Form>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={details.email} />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter first name" value={details.firstName} />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter last name" value={details.lastName} />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Phone number</Form.Label>
-                    <Form.Control type="tel" placeholder="Enter phone number" value={details.mobileNumber} />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="confrim Password" />
-                  </Form.Group>
+              {loading ? (
+                <Loader />
+              ) : (
+                details && (
+                  <Form>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control type="email" placeholder="Enter email" value={details.email} readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>First Name</Form.Label>
+                      <Form.Control type="text" placeholder="Enter first name" value={details.firstName} readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Last Name</Form.Label>
+                      <Form.Control type="text" placeholder="Enter last name" value={details.lastName} readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Phone number</Form.Label>
+                      <Form.Control type="tel" placeholder="Enter phone number" value={details.mobileNumber} readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control type="password" placeholder="Password" readOnly />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>confirm Password</Form.Label>
+                      <Form.Control type="password" placeholder="confrim Password" readOnly />
+                    </Form.Group>
 
-                  <Button variant="primary" type="submit">
-                    Update
-                  </Button>
-                </Form>
+                    <Button variant="primary" type="submit">
+                      Update
+                    </Button>
+                  </Form>
+                )
               )}
             </div>
           </div>
